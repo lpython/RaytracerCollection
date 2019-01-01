3b3009530f780b4b31e754b1a2557561bad1dd59
Alex M <amcnurlan@gmail.com>
2019-01-01 00:23:24 -0500

    Now start refactor

--- Python/scene_one.py
+++ Python/scene_one.py
@@ -1,18 +1,20 @@
-from raytrace import Scene, Camera
+from raytrace import Scene, Camera, Light
 from thing import Plane, Sphere
 import surface
+from vector import Vector
+from color import Color
 
 def scene():
   return Scene(things=[
-            Plane(  Vector(0.0, 1.0, 0.0), 0.0, surfaces.checkerboard),
-            Sphere( Vector(0.0, 1.0, -0.25), 1.0, surfaces.shiny),
-            Sphere( Vector(-1.0, 0.5, 1.5), 0.5, surfaces.shiny)
+            Plane(  Vector(0.0, 1.0, 0.0), 0.0, surface.checkerboard),
+            Sphere( Vector(0.0, 1.0, -0.25), 1.0, surface.shiny),
+            Sphere( Vector(-1.0, 0.5, 1.5), 0.5, surface.shiny)
         ],
         lights=[
-            { pos= Vector(-2.0, 2.5, 0.0), color= Color(0.49, 0.07, 0.07) },
-            { pos= Vector(1.5, 2.5, 1.5), color=Color(0.07, 0.07, 0.49) },
-            { pos= Vector(1.5, 2.5, -1.5), color= Color(0.07, 0.49, 0.071) },
-            { pos= Vector(0.0, 3.5, 0.0), color=Color(0.21, 0.21, 0.35) }
+            Light(Vector(-2.0, 2.5, 0.0), Color(0.49, 0.07, 0.07) ),
+            Light(Vector(1.5, 2.5, 1.5),  Color(0.07, 0.07, 0.49) ),
+            Light(Vector(1.5, 2.5, -1.5), Color(0.07, 0.49, 0.071) ),
+            Light(Vector(0.0, 3.5, 0.0),  Color(0.21, 0.21, 0.35))
         ],
         camera=Camera( Vector(3.0, 2.0, 4.0),  Vector(-1.0, 0.5, 0.0))
     )
